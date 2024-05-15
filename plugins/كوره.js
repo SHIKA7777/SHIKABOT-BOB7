a3598fda80565de8ac4f81b26ba337acb0d14942
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-15 14:26:19 +0300

    Update كوره.js

--- plugins/كوره.js
+++ plugins/كوره.js
@@ -7,19 +7,19 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/mohamedkun15/TheMystic-Bot-MD/master/src/JSON/Kora.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/uraharapp/3mk-urahara/main/URAHARA-3MK.JSON')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*╭━━━[ ${command.toUpperCase()} ]━━━━⬣*
-┃❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
-  *🦦لو مش عارف الاجابه قول استخدم.معرفش*
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
   ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-‌⎔ ━ • 𓆩♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕𓆪• ━ ⎔
+‌ ━ • 𓆩♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕𓆪• ━ 
      `.trim()
     conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img, '', caption, m),
+        await conn.sendFile(m.chat, json.question, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇╰━━━〔 *♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕* 〕━━━━━⬣`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]

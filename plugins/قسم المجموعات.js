2a98b407ec5c43d2618c8789fcf828fe9e70b278
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-17 22:37:15 +0300

    Update and rename menu-pdf.js to قسم المجموعات.js

---
+++ plugins/قسم المجموعات.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn }) => {
+    const name = conn.getName(m.sender);
+    let videoUrl = 'https://telegra.ph/file/18db853002c9bd7be317c.mp4';
+    let tagUser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let more = String.fromCharCode(8206);
+    let teks = `${pickRandom([`*╭─⚜️𝐖.𝐒.𝐙🔱𝐁𝐎𝐓⚜️─╮*
+*⎊———————————⎊*
+‌    *📜قســـم🔮الجـروب📜*
+*⎊———————————⎊*
+> *『منشن』┇🔮*
+> *『مخفي』┇🔮*
+> *『ترقيه』┇🔮*
+> *『اعفاء』┇🔮*
+> *『طرد』┇🔮*
+> *『اضافه』┇🔮*
+> *『الادمن』┇🔮*
+> *『جروب قفل』┇🔮*
+> *『جروب فتح』┇🔮* 
+> *『تغيير الاسم』┇🔮*
+*⎊———————————⎊*
+*⎊———————————⎊*
+*⎔ • توقيـــع الممـــلكه ✒️*
+*⎔* • 𝐖.𝐒.𝐙『🔱』𝐒𝐓𝐎𝐑𝐌`])}`.trim();
+
+    conn.sendMessage(m.chat, {
+        video: { url: videoUrl },
+        caption: teks,
+        gifPlayback: true,
+        gifAttribution: 0
+    }, { quoted: m });
+};
+
+handler.command = ['ق2','ق2'];
+
+export default handler;
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}

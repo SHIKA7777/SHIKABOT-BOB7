ccbc81ea36e05238c28b0b84a9651e821841da79
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-19 23:14:43 +0300

    Update and rename menu-downloader.js to قسم التحميلات.js

---
+++ plugins/قسم التحميلات.js
@@ -0,0 +1,40 @@
+let handler = async (m, { conn }) => {
+    const name = conn.getName(m.sender);
+    let videoUrl = 'https://telegra.ph/file/18db853002c9bd7be317c.mp4';
+    let tagUser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let more = String.fromCharCode(8206);
+    let teks = `${pickRandom([`*╭─⚜️𝐖.𝐒.𝐙🔱𝐁𝐎𝐓⚜️─╮*
+*⎊———————————⎊*
+‌    *📜قســـم📥التحميـل📜*
+*⎊———————————⎊*
+> *『يوتيوب』┇📥*
+> *『ايديت』┇📥*
+> *『تطبيق』┇📥*
+> *『اغنيه』┇📥*
+> *『شغل』┇📥*
+> *『صوره』┇📥*
+> *『بينت』┇📥*
+> *『فيد-فيس』┇📥*
+> *『تطقيم』┇📥* 
+> *『ميسي』┇📥*
+> *『رونالدو』┇📥* 
+*⎊———————————⎊*
+*⎊———————————⎊*
+*⎔ • توقيـــع الممـــلكه ✒️*
+*⎔* • 𝐖.𝐒.𝐙『🔱』𝐒𝐓𝐎𝐑𝐌`])}`.trim();
+
+    conn.sendMessage(m.chat, {
+        video: { url: videoUrl },
+        caption: teks,
+        gifPlayback: true,
+        gifAttribution: 0
+    }, { quoted: m });
+};
+
+handler.command = ['ق6','ق6'];
+
+export default handler;
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}

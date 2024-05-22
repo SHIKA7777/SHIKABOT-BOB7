1f2eeff1f2bd20d7eac3de9512fe4d1e4c447a0a
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 19:39:25 +0300

    Update رد بالصوت.js

--- plugins/رد بالصوت.js
+++ plugins/رد بالصوت.js
@@ -40,10 +40,10 @@ handler.all = async function(m, {conn}) {
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
 
-  if (!chat.isBanned && m.text.match(/(صباح الخير|صباح النور|صباح الفل|صباح الورد)/gi)) {
+  if (!chat.isBanned && m.text.match(/(رونالدو|كريستيانو)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/صباح_الخير.mp3';
+    const vn = './media/ماضر لو ودعتني ي رونالدو.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }

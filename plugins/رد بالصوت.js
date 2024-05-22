cf3b982ec524509ab470d7194a19eecc5bb94d61
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 19:42:20 +0300

    Update رد بالصوت.js

--- plugins/رد بالصوت.js
+++ plugins/رد بالصوت.js
@@ -43,7 +43,7 @@ handler.all = async function(m, {conn}) {
   if (!chat.isBanned && m.text.match(/(رونالدو|كريستيانو)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/ماضر لو ودعتني ي رونالدو.mp3';
+    const vn = './Media/ماضر لو ودعتني ي رونالدو.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }

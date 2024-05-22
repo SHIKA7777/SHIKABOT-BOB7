7f0678b41df0a4cdf198057ce53c2d7f3c970e54
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 06:39:31 +0300

    Update لوجو3.js

--- plugins/لوجو3.js
+++ plugins/لوجو3.js
@@ -2,11 +2,11 @@ import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
   if (!args[0]) throw 'مثال :\n*.lg5* bobiza'
-  m.reply('_إنتظر..._')
+  m.reply('*فاصبــر صبـرا جميــلا🤍*')
   let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
   conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming5'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
-handler.command = /^(lg5)$/i
+handler.command = /^(لوجو3)$/i
 export default handler

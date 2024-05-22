2588a314b71c3bf5727447267c0cbfdca61f3cb8
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 06:22:08 +0300

    Update and rename image.js to صوره2.js

--- plugins/صوره2.js
+++ plugins/صوره2.js
@@ -1,18 +1,18 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx']
-if (prohibited.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '🚩 *لن أعطي نتائج لطلبك* 😒', m, )
+if (prohibited.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '*مـش عــارف انـزلهــا*', m, )
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) throw `*تحميل صور من غوغل*\nمثــــال :\n*.image* facebook`
+if (!text) throw `*صـــورة ميســي بكـاس العــالم🦦*\nمثــــال :\n*.صوره2* facebook`
 
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
-conn.sendFile(m.chat, link, 'error.jpg', `📍 instagram.com/noureddine_ouafy` , m) 
+conn.sendFile(m.chat, link, 'error.jpg', `*تفـــضل ياحــب🤍*` , m) 
 }
 handler.help = ['image']
 handler.tags = ['downloader']
-handler.command = /^(image)$/i
+handler.command = /^(صوره2)$/i
 
 export default handler

34817b1296a8a1d3d4f914421193701e3e2a0bab
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 14:04:03 +0300

    Update and rename لوجو4.js to لوجو2.js

--- plugins/لوجو2.js
+++ plugins/لوجو2.js
@@ -2,12 +2,12 @@ import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'مثال :\n*.lg4* bobiza'
-  m.reply('_إنتظر..._')
+  m.reply('فاصبــر صبـرا جميــلا🤍')
   let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=${response[0]}`
   conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming4'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
-handler.command = /^(lg4)$/i
+handler.command = /^(لوجو2)$/i
 
 export default handler

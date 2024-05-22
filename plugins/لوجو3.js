71d49aeee792fb61e0cb8fcd75dfdea5520e37da
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 14:04:55 +0300

    Update and rename لوجو5.js to لوجو3.js

--- plugins/لوجو3.js
+++ plugins/لوجو3.js
@@ -2,12 +2,12 @@ import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'مثال :\n*.lg3* bobiza'
-  m.reply('_إنتظر..._')
+  m.reply('فاصبــر صبـرا جميــلا🤍')
   let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=${response[0]}`
   conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming3'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
-handler.command = /^(lg3)$/i
+handler.command = /^(لوجو3)$/i
 
 export default handler

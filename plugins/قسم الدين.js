59c065ce5e3c801a0625337fd8b5af7fcf94e893
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-22 14:20:21 +0300

    Update and rename menu-islam.js to قسم الدين.js

---
+++ plugins/قسم الدين.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn }) => {
+    const name = conn.getName(m.sender);
+    let videoUrl = 'https://telegra.ph/file/18db853002c9bd7be317c.mp4';
+    let tagUser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let more = String.fromCharCode(8206);
+    let teks = `${pickRandom([`*╭─⚜️𝐖.𝐒.𝐙🔱𝐁𝐎𝐓⚜️─╮*
+*⎊———————————⎊*
+‌    *📜قســـم🕌الديــن📜*
+*⎊———————————⎊*
+> *『قران』┇🕌*
+> *『اياتي』┇🕌*
+> *『الله』┇🕌*
+*⎊———————————⎊*
+*⎊———————————⎊*
+*⎔ • توقيـــع الممـــلكه ✒️*
+*⎔* • 𝐖.𝐒.𝐙『🔱』𝐒𝐓𝐎𝐑𝐌`])}`.trim();
+
+    conn.sendMessage(m.chat, {
+        video: { url: videoUrl },
+        caption: teks,
+        gifPlayback: true,
+        gifAttribution: 0
+    }, { quoted: m });
+};
+
+handler.command = ['ق9','ق9'];
+
+export default handler;
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}

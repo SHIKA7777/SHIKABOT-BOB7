7ce28fbe547072bca38883ad6a150582cf5cd372
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-23 12:54:23 +0300

    Update المهام.js

--- plugins/المهام.js
+++ plugins/المهام.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     const name = conn.getName(m.sender);
-    let videoUrl = 'https://telegra.ph/file/18db853002c9bd7be317c.mp4';
+    let videoUrl = 'https://telegra.ph/file/1ed771d1d8e8c03496f8b.mp4';
     let tagUser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let more = String.fromCharCode(8206);
     let teks = `${pickRandom([` *‌‌╮───⚜️المــ📜ـهــام⚜️──╭*

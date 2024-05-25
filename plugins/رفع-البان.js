d1ca818c646a55d201a7e9dd6e3676f94dab8350
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-25 11:01:02 +0300

    Update رفع-البان.js

--- plugins/رفع-البان.js
+++ plugins/رفع-البان.js
@@ -1,20 +1,15 @@
-//import db from '../lib/database.js'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-   let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    let user = global.db.data.users[who]
-    if (!who) throw `❒ منشن الشخص لفك البان`
-    let users = global.db.data.users
-    users[who].banned = false
-    conn.reply(m.chat, `
-تم الغاء البان !
-@${who.split`@`[0]} has been unbanned`, m, { mentions: [who] })
+let handler = async (m, { conn, text}) => {
+if (!text) throw '*[❗] متنساش المنشن يحب*'
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw '*[❗] متنساش المنشن يحب*'
+let users = global.db.data.users
+users[who].banned = false
+conn.reply(m.chat, `*[❗]تم إلغاء حظر المستخدم*\n*—◉ يقدر يستخدم البوت دلوقت*`, m)
 }
-handler.help = ['unban @user']
+handler.help = ['unbanuser']
 handler.tags = ['owner']
-handler.command = /^الغاء_البان$/i
+handler.command = /^رفع-البان$/i
 handler.rowner = true
-
 export default handler

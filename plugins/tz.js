b622f2e993b38f56999de824641de38d48dacd17
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-28 09:08:57 +0300

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -2,14 +2,13 @@ import moment from 'moment-timezone';
 
 const handler = async (m, {conn}) => {
 
-  const tzAF = moment().tz('Africa/Casablanca').format('DD/MM HH:mm');
+  const tzAF = moment().tz('Africa/$}{text} ').format('DD/MM HH:mm');
   await conn.sendMessage(m.chat, {text: `\`\`\`
 الوقت الحالي في المغرب هو :
 
 
-▢ morocco     : ${tzAF}
-  ${String.fromCharCode(8206).repeat(850)}
-  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+▢ morocco     : ${tz text}
+  ${String.fromCharCode(8206).repeat(850)`}, {quoted: m});
   };
 handler.help = ["tz"]
 handler.tags = ["infobot"]

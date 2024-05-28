b75b3864c667f08d755a5a8d045dfa5e61793da0
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-28 09:25:37 +0300

    Update translate.js

--- plugins/translate.js
+++ plugins/translate.js
@@ -14,7 +14,7 @@ text = args.join(' ')}
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 try {      
 let result = await translate(`${text}`, { to: lang, autoCorrect: true })
-await m.reply('*اتعلمــو بقــا وكفـايه قـرف🗿*' + result.text)
+await m.reply('*اتعلمــو بقــا وكفـايه قـرف🗿*' \n'التـرجمه' + result.text)
 } catch {
 try {    
 let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)

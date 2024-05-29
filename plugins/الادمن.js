55859efc776a8a1b41b7921a93406022cad66212
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-29 03:56:49 +0300

    Create الادمن.js

---
+++ plugins/الادمن.js
@@ -0,0 +1,24 @@
+const handler = async (m, {conn, participants, groupMetadata, args}) => {
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/admins.jpg';
+  const groupAdmins = participants.filter((p) => p.admin);
+  const listAdmin = groupAdmins.map((v, i) => `┃ ${i + 1}. @${v.id.split('@')[0]}`).join('\n');
+  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+  const pesan = args.join` `;
+  const oi = `*Mensaje:* ${pesan}`;
+  const text = `╭━〔  *استـــــدعـاء الادمـنـــز🦍*  〕⬣
+┃ ${oi}
+┃
+┃ *Admins:*
+${listAdmin}
+┃
+┃ 𓆩𝐖.𝐒.𝐙『🔱』𝐒𝐓𝐎𝐑𝐌𓆪
+╰━━━━━━━━━━━━⬣`.trim();
+  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
+};
+handler.help = ['admins <texto>'];
+handler.tags = ['group'];
+// regex detect A word without case sensitive
+handler.customPrefix = /a|@/i;
+handler.command = /^(admins|@admins|dmins|الادمن)$/i;
+handler.group = true;
+export default handler;

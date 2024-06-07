38800f769925477414a17f7dab7db32f000fbf82
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-06-07 21:55:10 +0300

    Update شطرنج.js‌

--- plugins/شطرنج.js
+++ plugins/شطرنج.js
@@ -16,75 +16,75 @@ const handler = async (m, { conn, args }) => {
 
   if (feature === 'حذف') {
     delete conn.chess[key];
-    return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *تم حذف اللعبه*', m);
+    return conn.reply(m.chat, '🏳️ *توقفت لعبة الشطرنج.*', m);
   }
 
-  if (feature === 'صمم') {
+  if (feature === 'تجهيز') {
     if (gameData) {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *اللعبه قائمه بالفعل*', m);
+      return conn.reply(m.chat, '⚠️ *لقد بدأت اللعبة.*', m);
     }
     chessData.gameData = { status: 'waiting', black: null, white: null };
-    return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *تم تصميم الغرقه*\nفي انتظار اللاعبين للانضمام.', m);
+    return conn.reply(m.chat, '🎮 *تبدأ لعبة الشطرنج.*\nفي انتظار انضمام لاعبين آخرين.', m);
   }
 
-  if (feature === 'ادخل') {
+  if (feature === 'انضمام') {
     const senderId = m.sender;
     if (players.includes(senderId)) {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *انت لقد قمت بالانضمام بالفعل*', m);
+      return conn.reply(m.chat, '🙅‍♂️ *لقد انضممت إلى اللعبة.*', m);
     }
     if (!gameData || gameData.status !== 'waiting') {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *لا يوجد لعبه شطرنج في انتظار اللاعبين حاليا*', m);
+      return conn.reply(m.chat, '⚠️ *لا توجد مباريات شطرنج معلقة.*', m);
     }
     if (players.length >= 2) {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *اكتمل عدد اللاعبين بالفعل.*\nاللعبه هتبدا لوحدها', m);
+      return conn.reply(m.chat, '👥 *هناك ما يكفي من اللاعبين.*\nتبدأ اللعبة تلقائيًا.', m);
     }
     players.push(senderId);
     hasJoined.push(senderId);
     if (players.length === 2) {
-      gameData.status = 'ready';
+      gameData.status = 'استعد';
       const [black, white] = Math.random() < 0.5 ? [players[1], players[0]] : [players[0], players[1]];
       gameData.black = black;
       gameData.white = white;
       chessData.currentTurn = white;
-      return conn.reply(m.chat, `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *اللاعبين الي اضمو:*\n${hasJoined.map(playerId => `- @${playerId.split('@')[0]}`).join('\n')}\n\n*الاسود:* @${black.split('@')[0]}\n*الابيض:* @${white.split('@')[0]}\n\nمن فضل استخدم *'شطرنج ابدا'* لبدا اللعبه.`, m, { mentions: hasJoined });
+      return conn.reply(m.chat, `🙌 *اللاعبون الذين انضموا:*\n${hasJoined.map(playerId => `- @${playerId.split('@')[0]}`).join('\n')}\n\n*Hitam:* @${black.split('@')[0]}\n*Putih:* @${white.split('@')[0]}\n\nSilakan gunakan *'chess start'* untuk memulai permainan.`, m, { mentions: hasJoined });
     } else {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』*انت لقد انضممت للعبه الشطرنج.*\nفي انتظار اللاعب الاخر', m);
+      return conn.reply(m.chat, '🙋‍♂️ *لقد انضممت إلى لعبة الشطرنج.*\nفي انتظار انضمام لاعبين آخرين.', m);
     }
   }
 
   if (feature === 'ابدا') {
-    if (gameData.status !== 'ready') {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *مش عارف ابدا اللعبه. لازم يخش لاعبين الثنين للعب*', m);
+    if (gameData.status !== 'استعد') {
+      return conn.reply(m.chat, '⚠️ *لا يمكن بدء اللعبة. انتظر حتى ينضم لاعبان.*', m);
     }
-    gameData.status = 'playing';
+    gameData.status = 'العب';
     const senderId = m.sender;
     if (players.length === 2) {
       const fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
       chessData.fen = fen;
       const encodedFen = encodeURIComponent(fen);
-      const turn = `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』دور: *الابيض* @${gameData.white.split('@')[0]}`;
+      const giliran = `🎲 *دور:* أبيض @${gameData.white.split('@')[0]}`;
       const flipParam = senderId === gameData.black ? '' : '&flip=true';
       const flipParam2 = senderId === gameData.black ? '' : '-flip';
       const boardUrl = `https://www.chess.com/dynboard?fen=${encodedFen}&board=graffiti&piece=graffiti&size=3&coordinates=inside${flipParam}`;
       try {
-        await conn.sendFile(m.chat, boardUrl, '', turn, m, false, { mentions: [gameData.white] });
+        await conn.sendFile(m.chat, boardUrl, '', giliran, m, false, { mentions: [gameData.white] });
       } catch (error) {
         const boardUrl2 = `https://chessboardimage.com/${encodedFen + flipParam2}.png`;
-        await conn.sendFile(m.chat, boardUrl2, '', turn, m, false, { mentions: [gameData.black] });
+        await conn.sendFile(m.chat, boardUrl2, '', giliran, m, false, { mentions: [gameData.black] });
       }
       return;
     } else {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *انت لقد انضممت الي اللعبه*\nفي انتظار اللاعب الثاني لدخول اللعبه', m);
+      return conn.reply(m.chat, '🙋‍♂️ *لقد انضممت إلى لعبة الشطرنج.*\nفي انتظار انضمام لاعبين آخرين.', m);
     }
   }
 
   if (args[0] && args[1]) {
     const senderId = m.sender;
     if (!gameData || gameData.status !== 'playing') {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *اللعبه لسه مبدأتش*', m);
+      return conn.reply(m.chat, '⚠️ *Permainan belum dimulai.*', m);
     }
     if (currentTurn !== senderId) {
-      return conn.reply(m.chat, `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *المفروض* ${chessData.currentTurn === gameData.white ? 'الابيض' : 'الاسود'}'هو الي يتحرك*`, m, {
+      return conn.reply(m.chat, `⏳ *Sekarang giliran ${chessData.currentTurn === gameData.white ? 'Putih' : 'Hitam'} untuk bergerak.*`, m, {
         contextInfo: {
           mentionedJid: [currentTurn]
         }
@@ -93,7 +93,7 @@ const handler = async (m, { conn, args }) => {
     const chess = new Chess(fen);
     if (chess.isCheckmate()) {
       delete conn.chess[key];
-      return conn.reply(m.chat, `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *كش مات☠️*\n*اللعبه خلصت.*\n*الكسبان 🎉:* @${m.sender.split('@')[0]}`, m, {
+      return conn.reply(m.chat, `⚠️ *Game Checkmate.*\n🏳️ *Permainan catur dihentikan.*\n*Pemenang:* @${m.sender.split('@')[0]}`, m, {
         contextInfo: {
           mentionedJid: [m.sender]
         }
@@ -101,7 +101,7 @@ const handler = async (m, { conn, args }) => {
     }
     if (chess.isDraw()) {
       delete conn.chess[key];
-      return conn.reply(m.chat, `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *تعادل 😑*\n*اللعبه خلصت*\n*الاعبين:* ${hasJoined.map(playerId => `- @${playerId.split('@')[0]}`).join('\n')}`, m, {
+      return conn.reply(m.chat, `⚠️ *Game Draw.*\n🏳️ *Permainan catur dihentikan.*\n*Pemain:* ${hasJoined.map(playerId => `- @${playerId.split('@')[0]}`).join('\n')}`, m, {
         contextInfo: {
           mentionedJid: hasJoined
         }
@@ -111,48 +111,48 @@ const handler = async (m, { conn, args }) => {
     try {
       chess.move({ from, to, promotion: 'q' });
     } catch (e) {
-      return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *الحركه غير صالحه*', m);
+      return conn.reply(m.chat, '❌ *Langkah tidak valid.*', m);
     }
     chessData.fen = chess.fen();
     const currentTurnIndex = players.indexOf(currentTurn);
     const nextTurnIndex = (currentTurnIndex + 1) % 2;
     chessData.currentTurn = players[nextTurnIndex];
     const encodedFen = encodeURIComponent(chess.fen());
-    const currentColor = chessData.currentTurn === gameData.white ? 'الابيض' : 'الاسود';
-    const turn = `『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』 *دور:* ${currentColor} @${chessData.currentTurn.split('@')[0]}\n\n${chess.getComment() || ''}`;
+    const currentColor = chessData.currentTurn === gameData.white ? 'Putih' : 'Hitam';
+    const giliran = `🎲 *Giliran:* ${currentColor} @${chessData.currentTurn.split('@')[0]}\n\n${chess.getComment() || ''}`;
     const flipParam = senderId === gameData.black ? '' : '&flip=true';
     const flipParam2 = senderId === gameData.black ? '' : '-flip';
     const boardUrl = `https://www.chess.com/dynboard?fen=${encodedFen}&board=graffiti&piece=graffiti&size=3&coordinates=inside${flipParam}`;
     try {
-      await conn.sendFile(m.chat, boardUrl, '', turn, m, false, { mentions: [chessData.currentTurn] });
+      await conn.sendFile(m.chat, boardUrl, '', giliran, m, false, { mentions: [chessData.currentTurn] });
     } catch (error) {
       const boardUrl2 = `https://chessboardimage.com/${encodedFen + flipParam2}.png`;
-      await conn.sendFile(m.chat, boardUrl2, '', turn, m, false, { mentions: [chessData.currentTurn] });
+      await conn.sendFile(m.chat, boardUrl2, '', giliran, m, false, { mentions: [chessData.currentTurn] });
     }
     chess.deleteComment();
     return;
   }
 
-  if (feature === 'مساعده') {
+  if (feature === 'help') {
     return conn.reply(m.chat, `
-      الشرح *مساعده اوامر لعبه الشطرنج:*
+      🌟 *Perintah Permainan Catur:*
 
-*شطرنج صمم* - عشان تعمل غرفه 
-*شطرنج ادخل* - عشان تنضم للغرفه الي صممتها انت و صحبك
-*شطرنج ابدا* - عشان تبدا الجيم الي انت وصحب عملتوه بعد ما دخلتو
-*شطرنج حذف* - عشان تطلع من اللعبه
-*شطرنج [من مكان القطعه الاولي] [الي المكان الي انت عايز تحط القطعه الثانيه]* - عشان تحرك القطع
+*chess create* - Mulai permainan catur
+*chess join* - Bergabung dalam permainan catur yang sedang menunggu
+*chess start* - Memulai permainan catur jika ada dua pemain yang sudah bergabung
+*chess delete* - Menghentikan permainan catur
+*chess [dari] [ke]* - Melakukan langkah dalam permainan catur
 
-*مثال:*
-اكتب *شطرنج صمم* عشان تعمل اللعبه.
-بعدها اكتب *شطرنج ادخل* عشان تخش انت وصاحبك اللعبه.
+*Contoh:*
+Ketik *chess create* untuk memulai permainan catur.
+Ketik *chess join* untuk bergabung dalam permainan catur yang sedang menunggu.
     `, m);
   }
-  return conn.reply(m.chat, '『𝔐𝔦𝔨𝔲 𝔅𝔒𝔗』“ امر خاطئ. استعمل*"شطرنج مساعده"* عشان تشوف الاوامر شغاله ازاي.', m);
+  return conn.reply(m.chat, '❓ Perintah tidak valid. Gunakan *"chess help"* untuk melihat bantuan.', m);
 };
 
-handler.help = ['chess [from to]', 'chess delete', 'chess join', 'chess start'];
+handler.help = ['chess [dari ke]', 'chess delete', 'chess join', 'chess start'];
 handler.tags = ['game'];
-handler.command = /^شطرنج|شت$/i
+handler.command = /^(شطرنج)$/i;
 
-export default handler
+export default handler;

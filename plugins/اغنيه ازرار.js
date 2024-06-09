29b0d330c008c81d9fd25d7aa371063dcdd1c709
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-06-09 12:37:54 +0300

    Update اغنيه ازرار.js

--- plugins/اغنيه ازرار.js
+++ plugins/اغنيه ازرار.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     ]
     let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
    // if (!lister.includes(feature)) return conn.reply(m.chat, `*🚩 Ingresa el formato en que deseas descargar más el titulo de un video o musica de YouTube.*\n\nEjemplo : ${usedPrefix + command} *mp3* SUICIDAL-IDOL - ecstacy\n\nFormatos disponibles :\n${usedPrefix + command} *mp3*\n${usedPrefix + command} *mp3doc*\n${usedPrefix + command} *mp4*\n${usedPrefix + command} *mp4doc*`,  m, fake,)
-          if (command == "اغنيه" || command == 'play2') {
+          if (command == "تشغيل" || command == 'play2') {
             if (!text) return conn.reply(m.chat, `*🚩 أدخل عنوان مقطع فيديو أو موسيقى*`,  m) 
     var res = await yts(text)
     var vid = res.videos[0]
@@ -245,6 +245,6 @@ thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
 }
 handler.help = ["play"].map(v => v + " <formato> <búsqueda>")
 handler.tags = ["downloader"]
-handler.command = ['play2', 'mp3', 'mp4', 'mp3doc', 'mp4doc']
+handler.command = ['تشغيل', 'play2', 'mp3', 'mp4', 'mp3doc', 'mp4doc']
 handler.star = 2
 export default handler
